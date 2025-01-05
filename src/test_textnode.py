c4dc1b216d547a4e4f8fa0747a420c78d0a1914c
tobii19-py <tobi19.py@gmail.com>
2025-01-04 21:37:11 -0600

    Implement unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,21 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_eq_norm(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        node2 = TextNode("This is a text node", TextType.NORMAL)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This isa text node", TextType.NORMAL)
+        self.assertNotEqual(node, node2)
+
+    def test_url_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
