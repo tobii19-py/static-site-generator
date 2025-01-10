7f3d0f587c2f90365eb989dd06003e729caa70e9
tobii19-py <tobi19.py@gmail.com>
2025-01-09 19:58:04 -0600

    Implement extract markdown links and image methods

--- src/md_to_textnode.py
+++ src/md_to_textnode.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     textnode_list = []
@@ -18,3 +19,25 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             textnode_list.append(node_item)
     
     return textnode_list
+
+def extract_markdown_images(text):
+    matched_alts = re.findall(r"\[(.*?)\]", text)
+    matched_urls = re.findall(r"\((.*?)\)", text)
+
+    md_imgs = []
+
+    for i in range(len(matched_alts)):
+        md_imgs.append((matched_alts[i], matched_urls[i]))
+
+    return md_imgs
+
+def extract_markdown_links(text):
+    matched_anchors = re.findall(r"\[(.*?)\]", text)
+    matched_urls = re.findall(r"\((.*?)\)", text)
+
+    md_links = []
+
+    for i in range(len(matched_anchors)):
+        md_links.append((matched_anchors[i], matched_urls[i]))
+
+    return md_links
