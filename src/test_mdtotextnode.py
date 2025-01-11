b72dc1094016439bc8c11530853098b5832cb86b
tobii19-py <tobi19.py@gmail.com>
2025-01-10 22:29:20 -0600

    Add tests for split nodes image

--- src/test_mdtotextnode.py
+++ src/test_mdtotextnode.py
@@ -1,4 +1,4 @@
-from md_to_textnode import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from md_to_textnode import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image
 from textnode import TextNode, TextType
 import unittest
 
@@ -48,3 +48,17 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             extract_markdown_links(text),
             [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
         )
+
+    def test_spltnd_imgs(self):
+        node = TextNode(
+            "This is text with an image of boots ![boots](https://www.boot.dev) and the goat ![kenpachi zaraki](https://static0.gamerantimages.com)",
+            TextType.NORMAL,
+        )
+        result = [
+            TextNode("This is text with an image of boots ", TextType.NORMAL),
+            TextNode("boots", TextType.IMAGES, "https://www.boot.dev"),
+            TextNode(" and the goat ", TextType.NORMAL),     
+            TextNode("kenpachi zaraki", TextType.IMAGES, "https://static0.gamerantimages.com")
+        ]
+        self.assertEqual(split_nodes_image([node]), result)
+
