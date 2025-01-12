1cc7005a111c04741a0fec0d8de999e1c319af53
tobii19-py <tobi19.py@gmail.com>
2025-01-12 10:42:59 -0600

    Implement block to block type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -5,3 +5,24 @@ def markdown_to_blocks(markdown):
         if len(block) > 0:
             new_blocks.append(block.strip())
     return new_blocks
+
+def block_to_block_type(block):
+    if block.startswith("#", 0, 5):
+        return f"'{block}' type is Heading"
+    if block.startswith("```") and block.endswith("```"):
+        return f"'{block}' type is Code"
+    
+    block_lines = block.split("\n")
+    quote = all(line.startswith(">") for line in block_lines)
+    if quote:
+        return f"'{block}' type is Quote"
+    
+    unordered_list = all(line.startswith("* ") or line.startswith("- ")
+                         for line in block_lines)
+    if unordered_list:
+        return f"'{block}' type is Unordered List"
+    
+    if all(line.startswith(f"{i+1}. ") for i, line in enumerate( block_lines)):
+        return f"'{block}' type is Ordered List"
+    else:
+        return f"'{block}' is a regular paragraph"
