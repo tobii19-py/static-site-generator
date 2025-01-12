a95f7619f9f70d849f231f6a42856466aec689fe
tobii19-py <tobi19.py@gmail.com>
2025-01-12 11:03:33 -0600

    Add tests for block to block type

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,4 +1,4 @@
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 import unittest
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -38,3 +38,36 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ]
         self.assertEqual(markdown_to_blocks(markdown), result)
+
+    def test_unordered_lists(self):
+        markdown = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        blocks = markdown_to_blocks(markdown)
+        
+        self.assertEqual(block_to_block_type(blocks[0]), 
+                         f"'{blocks[0]}' type is Heading")
+        
+        self.assertEqual(block_to_block_type(blocks[1]),
+                         f"'{blocks[1]}' is a regular paragraph")
+        
+        self.assertEqual(block_to_block_type(blocks[2]),
+                         f"'{blocks[2]}' type is Unordered List")
+        
+    def test_ordered_lists(self):
+        markdown = """
+1. This is first
+2. Second item
+3. Third item
+"""
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]),
+                         f"'{blocks[0]}' type is Ordered List")
+
+
