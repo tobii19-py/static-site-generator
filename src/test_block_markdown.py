2f3f06b4f7a275b26c46cde217e73ca02f3950da
tobii19-py <tobi19.py@gmail.com>
2025-01-11 23:50:17 -0600

    Add tests for block markdown

---
+++ src/test_block_markdown.py
@@ -0,0 +1,40 @@
+from block_markdown import markdown_to_blocks
+import unittest
+
+class TestBlockMarkdown(unittest.TestCase):
+    def test_mdn2_blocks(self):
+        markdown = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        result = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+        ]
+        self.assertEqual(markdown_to_blocks(markdown), result)
+
+    def test_mdn2_blocks_newlines(self):
+        markdown = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        result = [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ]
+        self.assertEqual(markdown_to_blocks(markdown), result)
