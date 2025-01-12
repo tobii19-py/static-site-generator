0942ebc9b5aac6269c1e0bd63afb6aaca98b7479
tobii19-py <tobi19.py@gmail.com>
2025-01-11 22:29:27 -0600

    Implement text to textnodes

--- src/md_to_textnode.py
+++ src/md_to_textnode.py
@@ -89,3 +89,15 @@ def split_nodes_link(old_nodes):
                 link_nodes.extend(remaining_links)
 
     return link_nodes
+
+def text_to_textnodes(text):
+
+    nodes = [TextNode(text, TextType.NORMAL)]
+
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+
+    return nodes
