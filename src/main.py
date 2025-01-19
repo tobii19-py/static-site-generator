b596ba8fb38e60eed662f923d015c4a330f4696a
tobii19-py <tobi19.py@gmail.com>
2025-01-18 20:27:10 -0600

    Create copy_static function

--- src/main.py
+++ src/main.py
@@ -1,19 +1,33 @@
 from textnode import TextNode, TextType
-import os
+import os, shutil
+
+def copy_static(src_path, dest_path):
+
+    if os.path.exists(src_path):
+        if os.path.exists(dest_path):
+            shutil.rmtree(dest_path)
+
+        os.mkdir(dest_path)
+        files = os.listdir(src_path)
+        print(files)
+        
+        for content in files:
+            file = os.path.isfile(os.path.join(src_path, content))
+            print(file)
+            if file:
+                shutil.copy2(file, dest_path)
+            
+    else:
+        raise ValueError("Invalid: Enter a valid source directory")
 
 def main():
     test = TextNode("This is a test", TextType.BOLD, "https://www.boot.dev")
     print(test)
 
-    src_path = "static"
-    dest_path = "public"
-    if os.path.exists(src_path):
-        if os.path.exists(dest_path):
-            print(dest_path)
-        else:
-            raise ValueError("Invalid: Enter a valid destination directory")
-    else:
-        raise ValueError("Invalid: Enter a valid source directory")
+    src = "static"
+    dest = "public"
+    copy_static(src, dest)
+
     
 
 if __name__ == "__main__":
