0aafa49834a5d430ae39d5c12f00afdcbb01f626
tobii19-py <tobi19.py@gmail.com>
2025-01-18 19:50:58 -0600

    Check if path exists

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from textnode import TextNode, TextType
+import os
 
 def main():
     test = TextNode("This is a test", TextType.BOLD, "https://www.boot.dev")
     print(test)
+
+    src_path = "static"
+    dest_path = "public"
+    if os.path.exists(src_path):
+        if os.path.exists(dest_path):
+            print(dest_path)
+        else:
+            raise ValueError("Invalid: Enter a valid destination directory")
+    else:
+        raise ValueError("Invalid: Enter a valid source directory")
+    
 
 if __name__ == "__main__":
     main()
