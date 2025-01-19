41503dca4ea38fc3ba2532d0bfc5f251d4f00945
tobii19-py <tobi19.py@gmail.com>
2025-01-18 20:59:56 -0600

    Copy files from root and subdirectory to destination directory

--- src/main.py
+++ src/main.py
@@ -2,31 +2,38 @@ from textnode import TextNode, TextType
 import os, shutil
 
 def copy_static(src_path, dest_path):
+    copied_paths = []
 
     if os.path.exists(src_path):
         if os.path.exists(dest_path):
             shutil.rmtree(dest_path)
 
         os.mkdir(dest_path)
-        files = os.listdir(src_path)
-        print(files)
+        items = os.listdir(src_path)
         
-        for content in files:
-            file = os.path.isfile(os.path.join(src_path, content))
-            print(file)
-            if file:
+        for item in items:
+            file = os.path.join(src_path, item)
+
+            if os.path.isfile(file) == True:
                 shutil.copy2(file, dest_path)
-            
+                copied_paths.append(file)
+            else:
+                dest_path = os.path.join(dest_path, item)
+                cp = copy_static(file, dest_path)
+                copied_paths.extend(cp)         
     else:
         raise ValueError("Invalid: Enter a valid source directory")
+    
+    return copied_paths
 
 def main():
-    test = TextNode("This is a test", TextType.BOLD, "https://www.boot.dev")
-    print(test)
-
     src = "static"
     dest = "public"
-    copy_static(src, dest)
+    copied_files = copy_static(src, dest)
+    print("\nFiles copied:")
+
+    for file in copied_files:
+        print(f" - {file}")
 
     
 
